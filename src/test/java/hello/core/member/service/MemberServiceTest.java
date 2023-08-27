1f0853966b4426851c25c39a31d6c92fa439752a
정서현 <tjgus027@kakao.com>
2023-08-27 23:01:30 +0900

    섹션 2 - 회원 도메인 설계 및 테스트 (순수 자바로만)

---
+++ src/test/java/hello/core/member/service/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package hello.core.member.service;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    private MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(member.getId());
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}

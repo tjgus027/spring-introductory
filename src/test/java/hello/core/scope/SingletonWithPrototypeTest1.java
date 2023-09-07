f5bfdbee349a647c8fc6a4ff2412fd69307f201d
정서현 <tjgus027@kakao.com>
2023-09-07 18:46:32 +0900

    섹션 9 - 싱글톤과 프로토타입 동시 사용 시 문제와 해결 테스트

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,7 +1,9 @@
 package hello.core.scope;
 
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -36,20 +38,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
-//    @Component 이걸 붙이면 생성자 필요 X
+    @Component
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
-
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider = new Provider<PrototypeBean>() {
+    @Override
+    public PrototypeBean get() {
+        return new PrototypeBean();
+    }
+};
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
